aca7d23fd497545dfcea186e189578c6964b0d0b
Manojkapure100 <manojkapure100@gmail.com>
2023-02-26 15:20:53 +0530

    changed3

--- java1.java
+++ java1.java
@@ -2,6 +2,5 @@ class java1 {
     public static void main(String args[]) {
         System.out.println("Hello from Java1");
         System.out.println("Calling jenkins");
-        System.out.println("Calling jenkins success");
     }
 }
